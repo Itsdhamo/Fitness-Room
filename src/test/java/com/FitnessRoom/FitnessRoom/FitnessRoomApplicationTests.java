e5b16ed9b87e27c1eb76dedc2eafff382aef0717
itsdhamo <dhamodaran369@gmail.com>
2024-05-18 11:18:34 +0530

    INITIAL STAGE

---
+++ src/test/java/com/FitnessRoom/FitnessRoom/FitnessRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FitnessRoom.FitnessRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
